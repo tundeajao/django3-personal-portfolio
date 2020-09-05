5096ccd3f0901a8d965ae412d7096339554bc231
Tunde Ajao <tunde_ajao@hotmail.com>
2020-09-05 19:01:33 +0100

    Added an apps blog

--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def all_blogs(request):
+    return render(request, 'blog/all_blogs.html')
+
+def Tunde(request):
+    return HttpResponse('THIS IS IT')
