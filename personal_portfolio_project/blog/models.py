de305c5bc00a446eb6a04b01dd3c30434ff9ab93
Tunde Ajao <tunde_ajao@hotmail.com>
2020-09-06 17:48:44 +0100

    Po;ishing the code and minor correction and updates

--- personal_portfolio_project/blog/models.py
+++ personal_portfolio_project/blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     dateStamp =  models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.title
